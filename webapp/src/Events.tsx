60bed5e770dc61178b507ab1c918ef306ae7f55f
Erik Reid <erik.reid@geant.org>
2022-04-22 14:40:57 +0200

    cleanup & refresh whole screen to reset day

--- webapp/src/Events.tsx
+++ webapp/src/Events.tsx
@@ -1,81 +1,18 @@
 import React, {useEffect} from 'react';
-// import { formatDistance } from 'date-fns';
 import {
     Calendar,
     Views,
-    // DateLocalizer,
     momentLocalizer,
   } from 'react-big-calendar'
 import "./styles.scss"
-// import PropTypes from 'prop-types'
 import moment from 'moment'
 
-const events_url = "http://localhost:33333/api/events";
+const EVENTS_API_ENDPOINT = "api/events";
 
-// interface Event {
-//     id: string;
-//     data1: string;
-//     data2: string;
-//     data3: string;
-//     state: boolean;
-//     time: number;
-// }
-
-
-// export interface IAppProps {
-//     refresh_frequency_ms?: number;
-//     api_url?: string;
-// }
-
-// function timeFormatter(time: number, _thing: Thing): string {
-//     return formatDistance(new Date(1000 * time), new Date());
-// }
-
-// const columns = [
-//     {
-//         dataField: 'state',
-//         text: 'State'
-//     },
-//     {
-//         dataField: 'data1',
-//         text: 'Data 1'
-//         // title: columnTitle
-//     }, {
-//         dataField: 'data2',
-//         text: 'Data 2'
-//     }, {
-//         dataField: 'data3',
-//         text: 'Data 3',
-//         // classes: severityClasses,
-//         // formatter: severityFormatter
-//     }, {
-//         dataField: 'time',
-//         text: 'When',
-//         formatter: timeFormatter
-//     }
-// ];
-
-
-// const ThingRow = (t: Thing): Thing => {
-//     return {
-//         id: t.id,
-//         time: t.time,
-//         state: t.state,
-//         data1: t.data1,
-//         data2: t.data2,
-//         data3: t.data3
-//     }
-// }
-
-
-// const rowClass = (row: Thing, _rowIndex: number): string => {
-//     if (row.state)
-//         return 'state_true';
-//     else
-//         return 'state_false';
-// }
-
-
+export interface IAppProps {
+    api_uri_base: string;
+    refresh_ms: number;
+}
 
 interface Event {
     id: number;
@@ -102,37 +39,10 @@ const make_event = (e: InputEvent): Event => {
     };
 };
 
-// const the_localizer = PropTypes.instanceOf(DateLocalizer);
 const the_localizer = momentLocalizer(moment);
-
-
-// const ColoredDateCellWrapper = (children: any) =>
-//   React.cloneElement(React.Children.only(children), {
-//     style: {
-//       backgroundColor: 'lightblue',
-//     },
-//   })
-
-// const the_components = {
-//     timeSlotWrapper: ColoredDateCellWrapper,
-// };
-
-const the_views = [Views.MONTH, Views.WEEK, Views.DAY];
-
-// const { components, defaultDate, max, views } = useMemo(
-//     () => ({
-//       components: {
-//         timeSlotWrapper: ColoredDateCellWrapper,
-//       },
-//       defaultDate: new Date(2015, 3, 1),
-//       max: dates.add(dates.endOf(new Date(2015, 17, 1), 'day'), -1, 'hours'),
-//       views: Object.keys(Views).map((k) => Views[k]),
-//     }),
-//     []
-//   )
-
-
-const Events: React.FC = () => {
+const the_views = [Views.MONTH];
+// const the_views = [Views.MONTH, Views.WEEK, Views.DAY];
+const Events: React.FC<IAppProps> = (props) => {
 
     const [events, setEvents] = React.useState<Event[]>([]);
     
@@ -140,21 +50,19 @@ const Events: React.FC = () => {
 
     useEffect(() => {
 
+        function reload() {
+            window.location.reload();
+        }
+
         async function loadEvents() {
-            // console.log('starting loadEvents(), source: "' + props.api_url + '"');
-            
-            // api_uri shouldn't be undefined, since
-            // there's a default value - but TypeScript can't see
-            // this, apparently (?)
-            const response = await fetch(events_url);
+            const response = await fetch(props.api_uri_base + EVENTS_API_ENDPOINT);
             const rsp_json = await response.json();
             setEvents(rsp_json.map((e: InputEvent) => make_event(e)));
         }
 
         loadEvents();
-        setInterval(loadEvents, 60000);
-
-    }, []);
+        setInterval(reload, props.refresh_ms);
+    }, [props.refresh_ms, props.api_uri_base]);
 
     return <div className="height600">
         <Calendar
@@ -162,6 +70,7 @@ const Events: React.FC = () => {
             events={events}
             localizer={the_localizer}
             views={the_views}
+            // toolbar={false}
         />
     </div>;
 };
--- webapp/src/index.tsx
+++ webapp/src/index.tsx
@@ -2,8 +2,19 @@ import React from "react";
 import ReactDOM from "react-dom";
 import Events from "./Events"
 
-// const query = new URLSearchParams(window.location.search);
+const query = new URLSearchParams(window.location.search);
 
-ReactDOM.render(
-  <Events />,
-  document.getElementById("root") as HTMLElement);
+if (query.has('test'))
+  ReactDOM.render(
+    <Events 
+      api_uri_base='http://localhost:33333/'
+      refresh_ms={60*1000}
+    />,
+    document.getElementById("root") as HTMLElement);
+else
+  ReactDOM.render(
+    <Events 
+      api_uri_base={window.location.origin}
+      refresh_ms={12*3600*1000}
+    />,
+    document.getElementById("root") as HTMLElement);
