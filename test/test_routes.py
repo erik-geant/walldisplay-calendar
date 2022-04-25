0b0119dcf3522374330b9638ccb9cd3d86b5c900
Erik Reid <erik.reid@geant.org>
2022-04-25 16:41:11 +0200

    added test checking duration < 7d

--- test/test_routes.py
+++ test/test_routes.py
@@ -2,13 +2,14 @@ import json
 import os
 import re
 
+from dateutil.parser import isoparse
 import jsonschema
 import pytest
 import responses
 
 from walldisplay_calendar.routes.default import VERSION_SCHEMA
-from walldisplay_calendar.routes.api import EVENT_LIST_SCHEMA
-
+from walldisplay_calendar.routes.api \
+    import EVENT_LIST_SCHEMA, EMS_LONG_EVENT_THRESHOLD_S
 
 @pytest.mark.parametrize(
     'endpoint',
@@ -45,5 +46,20 @@ def test_events(client):
         'api/events',
         headers={'Accept': ['application/json']})
     assert rv.status_code == 200
-    result = json.loads(rv.data.decode('utf-8'))
-    jsonschema.validate(result, EVENT_LIST_SCHEMA)
+    events = json.loads(rv.data.decode('utf-8'))
+    jsonschema.validate(events, EVENT_LIST_SCHEMA)
+    assert events  # sanity: test data has at least one trap
+
+    def _duration(e):
+        start = isoparse(e['startDate'])
+        end = isoparse(e['endDate'])
+        return (end - start).total_seconds()
+
+    x = list(map(
+        lambda e: _duration(e) <= EMS_LONG_EVENT_THRESHOLD_S,
+        events))
+    print(x)
+
+    assert all(map(
+        lambda e: _duration(e) <= EMS_LONG_EVENT_THRESHOLD_S,
+        events))
